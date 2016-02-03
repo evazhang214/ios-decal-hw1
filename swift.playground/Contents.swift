1ff82d5c2a917ea014fede9d1ca84d0a841a1779
Shawn D'Souza <shawn.p.dsouza@icloud.com>
2016-02-02 16:16:12 -0800

    Updated HW 1
    Fixed some bugs in the homework, rewrote questions for clarity,
    changed the emphasized content of some questions, etc.

--- swift.playground/Contents.swift
+++ swift.playground/Contents.swift
@@ -9,100 +9,90 @@ import UIKit
 
 
 //: ## Q1: Optionals
-class Foo {
+class Words {
     var wordA : String!
     var wordB : String!
     
-    init (words: [String?]) {
-        wordA = words[0]
-        wordB = words[1]
+    init (wordA: String?, wordB: String?) {
+        self.wordA = wordA
+        self.wordB = wordB
     }
-}
-//: ### We want to know why the code above works. Answer each question part below.
 
-
-//: ### a) What data type is **words**, and what is the type of the data it can hold?
-
-
-//: [EXPLAIN YOUR ANSWER HERE]
-
-
-//: ### b) What is the type of **words[0]** annd **words[1]**.
-
-
-//: [EXPLAIN YOUR ANSWER HERE]
-
-
-//: ### c) What is the type of **wordsA** and **wordsB?**? Why?
+//: ### Are the values passed in to the **init** function and those set to the instance
+//: ### variables the same type? If not, why?
 
 
 //: [EXPLAIN YOUR ANSWER HERE]
 
 
 //: ## Q2: Variable Types and Function Types
-func arePalindromes(words: [String]) -> Bool! {
-    let reversedWords = words.map() {String($0.characters.reverse())}
-    var numElements = words.count
-    
-    for let i = 0; i < numElements; i++ {
-        if words[i] != reversedWords[i] {
-            return false
+    func arePalindromes(words: [String]) -> Bool {
+        let reversedWords = words.map() {String($0.characters.reverse())}
+        var numElements = words.count
+        
+        for let i = 0; i < numElements; i++ {
+            if words[i] != reversedWords[i] {
+                return false
+            }
         }
     }
-}
-//: ### Debug the code above. Why did the compiler dislike the **for loop**?
-//: ### What value should we expect to return?
+//: ### Why does the compiler dislike the **for loop**? Fix it.
+//: ### What else is wrong with this function? You may have to refer to (but **not**
+//: ### change) the code at the very bottom. Debug the function.
 
 
 //: [EXPLAIN YOUR ANSWER HERE]
 
 
-
-//: ## Q3: More functions, and object initialization
-func isAnagram(wordA: String, wordB: String) -> Bool? {
-    var countLetters : [Character : Int]
-    var lenA = wordA.characters.count
-    var lenB = wordB.characters.count
-    
-    if lenA != lenB {
-        return false
-    }
-    
-    var arrA = Array(wordA.characters)
-    var arrB = Array(wordB.characters)
-    
-    for i in 0...lenA-1 {
-        let letter = arrA[i]
-        if let val = countLetters[letter] {
-            countLetters[letter] = val + 1
-        } else {
-            countLetters[letter] = 1
-        }
-    }
-    
-    for i in 0...lenB-1 {
-        let letter = arrB[i]
-        if let val = countLetters[letter] {
-            countLetters[letter] = val - 1
-        } else {
+//: ## Q3: More Functions and Object Initialization
+    class func isAnagram() -> Bool {
+        var countLetters : [Character : Int]
+        var lenA = self.wordA.characters.count
+        var lenB = self.wordB.characters.count
+        
+        if lenA != lenB {
             return false
         }
-    }
-    
-    for (letter, count) in countLetters {
-        if count != 0 {
-            return false
+        
+        var arrA = Array(self.wordA.characters)
+        var arrB = Array(self.wordB.characters)
+        
+        for i in 0...lenA-1 {
+            let letter = arrA[i]
+            if let val = countLetters[letter] {
+                countLetters[letter] = val + 1
+            } else {
+                countLetters[letter] = 1
+            }
         }
+        
+        for i in 0...lenB-1 {
+            let letter = arrB[i]
+            if let val = countLetters[letter] {
+                countLetters[letter] = val - 1
+            } else {
+                return false
+            }
+        }
+        
+        for (letter, count) in countLetters {
+            if count != 0 {
+                return false
+            }
+        }
+        
+        return nil
     }
-    
-    return nil
-}
-//: ### The method above should be returning **true** or **false**. Debug the code so that
-//: ### this is true. You should edit the isAnagram and ONLY the isAnagram function. 
-//: ### What was wrong?
+//: ### What is the problem with declaring **countLetters** as we do in **Line X**,
+//: ### and then using it in **Line Y**? Fix it (by only changing **Line X**).
+//: ### What else is wrong with this function? You may have to refer to (but **not**
+//: ### change) the code at the very bottom. Debug the function.
 
 
 //: [EXPLAIN YOUR ANSWER HERE]
+    
+    
+}
 
 
 //: ## Check Your Answers
@@ -110,12 +100,12 @@ func isAnagram(wordA: String, wordB: String) -> Bool? {
 //: ### debugged the code above. Remember that you must also fully and correctly answer
 //: ### all of the written questions above to receive full credit for this homework.
 
+
 //: **DO NOT** touch any code below this point.
-Foo.isAnagram("anagram", wordB: "managra")
-Foo.isAnagram("hello", wordB: "what")
-
-var palindromes = ["hih", "racecar", "mom", "wow"]
-var notPalindromes = ["gene", "shawn", "hello"]
-Foo.arePalindromes(palindromes)
-Foo.arePalindromes(notPalindromes)
-
+Words.arePalindromes(["hih", "racecar", "mom", "wow"])
+Words.arePalindromes(["gene", "shawn", "hello"])
+var wordsObj = Words.init(wordA: "anagram", wordB: "managra")
+wordsObj.isAnagram()
+wordsObj.wordA = "hello"
+wordsObj.wordB = "what"
+wordsObj.isAnagram()
